ce1c815f17fe3c4d39a173f19c52184677a26d06
Pelayo Rojas Íñigo <pelayori2003@gmail.com>
2024-02-07 11:46:25 +0100

    Create Spring project

---
+++ src/main/java/com/uniovi/wiq_es04b/WiqEs04bApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.wiq_es04b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiqEs04bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiqEs04bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/wiq_es04b/WiqEs04bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.wiq_es04b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiqEs04bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
