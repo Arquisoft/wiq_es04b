9e56ec07e194a4cef1981a4a490326ec4143cdca
Pelayo Rojas Íñigo <pelayori2003@gmail.com>
2024-02-11 21:06:22 +0100

    Error page, needs fix

--- src/main/java/com/uniovi/controllers/CustomErrorController.java
+++ src/main/java/com/uniovi/controllers/CustomErrorController.java
@@ -1,4 +1,48 @@
 package com.uniovi.controllers;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-public class CustomErrorController {
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class CustomErrorController extends BasicErrorController {
+
+    private static final String PATH = "/error";
+    private final ErrorProperties errorProperties;
+
+    @Autowired
+    public CustomErrorController(ErrorAttributes errorAttributes, ErrorProperties errorProperties) {
+        this(errorAttributes, errorProperties, Collections.emptyList());
+    }
+
+    public CustomErrorController(ErrorAttributes errorAttributes, ErrorProperties errorProperties, List<ErrorViewResolver> errorViewResolvers) {
+        super(errorAttributes, errorProperties);
+        Assert.notNull(errorProperties, "ErrorProperties must not be null");
+        this.errorProperties = errorProperties;
+    }
+
+    @RequestMapping(value = PATH)
+    public String error(Model model, HttpServletRequest webRequest) {
+        Map<String, Object> errorAttributes = null;
+        model.addAttribute("error", errorAttributes.get("error"));
+        model.addAttribute("message", errorAttributes.get("message"));
+        model.addAttribute("status", errorAttributes.get("status"));
+        model.addAttribute("trace", errorAttributes.get("trace")); // Add the stack trace
+        return "error";
+    }
 }
