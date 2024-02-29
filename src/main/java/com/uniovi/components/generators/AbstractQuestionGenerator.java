f355ca04ce62ced78c4cde1928a332772fbd9a1f
uo287545 <uo287545@uniovi.es>
2024-02-29 03:50:41 +0100

    Improvements in the question generation system

--- src/main/java/com/uniovi/components/generators/AbstractQuestionGenerator.java
+++ src/main/java/com/uniovi/components/generators/AbstractQuestionGenerator.java
@@ -1,15 +1,25 @@
 package com.uniovi.components.generators;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uniovi.entities.Answer;
 import com.uniovi.entities.Category;
 import com.uniovi.entities.Question;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractQuestionGenerator implements QuestionGenerator{
 
-    private List<Question> questions;
+    private List<Question> questions = new ArrayList<>();
+    private String query;
+    protected String statement;
 
     public void questionGenerator(String statement, List<String> options, String correctAnswer, Category category){
         List<Answer> answers = new ArrayList<>();
@@ -23,11 +33,51 @@ public abstract class AbstractQuestionGenerator implements QuestionGenerator{
         answers.add(correct);
 
         Question question = new Question(statement, answers, correct, category);
+        question.scrambleOptions();
         System.out.println(question);
+        questions.add(question);
     }
 
     public List<Question> getQuestions() {
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+
+            String endpointUrl = "https://query.wikidata.org/sparql?query=" +
+                    URLEncoder.encode(this.getQuery(), StandardCharsets.UTF_8.toString()) +
+                    "&format=json";
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endpointUrl))
+                    .header("Accept", "application/json")
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Process the JSON response using Jackson ObjectMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonResponse = objectMapper.readTree(response.body());
+
+            // Access the data from the JSON response
+            JsonNode resultsNode = jsonResponse.path("results").path("bindings");
+
+            for (JsonNode result : resultsNode) {
+
+                List<String> options = this.generateOptions(resultsNode, result);
+                String correctAnswer = this.generateCorrectAnswer(result);
+                String statement = this.getQuestionSubject(result);
+                questionGenerator(statement, options, correctAnswer, this.getCategory());
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return questions;
     }
+
+    protected abstract List<String> generateOptions(JsonNode results, JsonNode result);
+    protected abstract String generateCorrectAnswer(JsonNode result);
+
+    protected abstract String getQuestionSubject(JsonNode result);
 
 }
--- src/main/java/com/uniovi/components/generators/geography/CapitalQuestionGenerator.java
+++ src/main/java/com/uniovi/components/generators/geography/CapitalQuestionGenerator.java
@@ -1,12 +1,16 @@
 package com.uniovi.components.generators.geography;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class CapitalQuestionGenerator extends AbstractGeographyGenerator{
+
+    public CapitalQuestionGenerator() {
+        this.statement = "What is the capital of ";
+    }
+
     @Override
     public String getQuery() {
         return "SELECT DISTINCT ?country ?countryLabel ?capital ?capitalLabel\n" +
@@ -33,7 +37,6 @@ public class CapitalQuestionGenerator extends AbstractGeographyGenerator{
     }
 
     private List<String> selectRandomIncorrectCapitals(List<String> allCapitals, String correctCapital, int count) {
-        // Seleccionar aleatoriamente tres capitales incorrectas distintas a la capital correcta
         List<String> incorrectCapitals = new ArrayList<>();
         Random random = new Random();
         while (incorrectCapitals.size() < count && allCapitals.size() > 0) {
@@ -44,5 +47,22 @@ public class CapitalQuestionGenerator extends AbstractGeographyGenerator{
             }
         }
         return incorrectCapitals;
+    }
+
+    @Override
+    protected List<String> generateOptions(JsonNode results, JsonNode result) {
+        String countryLabel = result.path("countryLabel").path("value").asText();
+        String capitalLabel = result.path("capitalLabel").path("value").asText();
+        return selectRandomIncorrectCapitals(getAllCapitals(results, countryLabel, capitalLabel), capitalLabel, 3);
+    }
+
+    @Override
+    protected String generateCorrectAnswer(JsonNode result) {
+        return result.path("capitalLabel").path("value").asText();
+    }
+
+    @Override
+    protected String getQuestionSubject(JsonNode result) {
+        return this.statement + result.path("countryLabel").path("value").asText() + "?";
     }
 }
