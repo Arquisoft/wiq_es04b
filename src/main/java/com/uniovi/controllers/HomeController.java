94b0443fb4bc663701aa87d6adfc3fd0ce2eb547
Iker Álvarez Fernández <uo287545@uniovi.es>
2024-03-05 23:36:49 +0100

    Cambios iniciales realizados en clase

--- src/main/java/com/uniovi/controllers/HomeController.java
+++ src/main/java/com/uniovi/controllers/HomeController.java
@@ -1,21 +1,28 @@
 package com.uniovi.controllers;
 
 import com.uniovi.services.PlayerService;
+import com.uniovi.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Random;
 
 @Controller
 public class HomeController{
     private final PlayerService playerService;
+    private final QuestionService questionService;
 
     @Autowired
-    public HomeController(PlayerService playerService) {
+    public HomeController(PlayerService playerService, QuestionService questionService) {
         this.playerService = playerService;
+        this.questionService = questionService;
     }
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("question", questionService.getAllQuestions().get(new Random().nextInt(questionService.getAllQuestions().size())));
         return "index";
     }
 
