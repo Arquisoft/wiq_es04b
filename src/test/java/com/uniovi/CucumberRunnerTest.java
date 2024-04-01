be5129bcd361355a93d7dd478beb591293ac567e
Pelayori <31128562+Pelayori@users.noreply.github.com>
2024-04-01 17:50:44 +0200

    Test run integration tests

--- src/test/java/com/uniovi/CucumberRunnerTest.java
+++ src/test/java/com/uniovi/CucumberRunnerTest.java
@@ -3,8 +3,10 @@ package com.uniovi;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features")
+@SpringBootTest
 public class CucumberRunnerTest {
 }
