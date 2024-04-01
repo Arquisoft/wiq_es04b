6d507bb5211c8a029aa7d5c014232bcce7f7d4f6
Pelayori <31128562+Pelayori@users.noreply.github.com>
2024-04-01 23:18:02 +0200

    Add caching back

--- src/test/java/com/uniovi/Wiq_UnitTests.java
+++ src/test/java/com/uniovi/Wiq_UnitTests.java
@@ -1,6 +1,5 @@
 package com.uniovi;
 
-import com.uniovi.dto.PlayerDto;
 import com.uniovi.entities.Player;
 import com.uniovi.services.PlayerService;
 import org.junit.jupiter.api.*;
@@ -8,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import java.util.List;
 
 import java.util.Optional;
 
@@ -23,14 +23,7 @@ public class Wiq_UnitTests {
     @Test
     @Order(1)
     public void testPlayerService() {
-//        PlayerDto dto = new PlayerDto();
-//        dto.setUsername("test");
-//        dto.setPassword("test");
-//        dto.setEmail("test@test.com");
-//        dto.setRoles(new String[]{"ROLE_USER"});
-//        Player player = playerService.addNewPlayer(dto);
-//        Assertions.assertNotNull(player);
-//        Optional<Player> playerOptional = playerService.getUser(player.getId());
-//        Assertions.assertTrue(playerOptional.isPresent());
+        List<Player> players = playerService.getUsersByRole("ROLE_USER");
+        Assertions.assertEquals(0, players.size());
     }
 }
