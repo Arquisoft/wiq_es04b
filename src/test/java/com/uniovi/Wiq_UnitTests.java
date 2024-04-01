9973a97aa4c3ae44e96efe405faa423655871466
Pelayori <31128562+Pelayori@users.noreply.github.com>
2024-04-01 19:14:13 +0200

    Test run integration tests

--- src/test/java/com/uniovi/Wiq_UnitTests.java
+++ src/test/java/com/uniovi/Wiq_UnitTests.java
@@ -8,15 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
-//@Tag("unit")
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//@ActiveProfiles("test")
-class Wiq_UnitTests {
+@SpringBootTest
+@Tag("unit")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+public class Wiq_UnitTests {
 
-    //@Test
-    void contextLoads() {
+    @Test
+    public void contextLoads() {
 
     }
 
