85b27744fffe0b75f3c8e72aeede96b3f6c8cf1b
UO287568 <uo287568@uniovi.es>
2024-04-01 17:53:43 +0200

    PlayerServiceImpl.addNewPlayer() unit tests

--- src/test/java/com/uniovi/Wiq_UnitTests.java
+++ src/test/java/com/uniovi/Wiq_UnitTests.java
@@ -1,10 +1,14 @@
 package com.uniovi;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.uniovi.dto.*;
+import com.uniovi.entities.*;
+import com.uniovi.repositories.*;
+import com.uniovi.services.*;
+import com.uniovi.services.impl.*;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -15,8 +19,78 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class Wiq_UnitTests {
 
+    @Autowired
+    PlayerRepository playerRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @Order(1)
+    void PlayerServiceImpl_addNewPlayer_UsedEmail() {
+        RoleService roleService = new RoleServiceImpl(roleRepository);
+        PlayerServiceImpl playerService = new PlayerServiceImpl(playerRepository, roleService, passwordEncoder);
+
+        Player p1 = new Player("a", "abcd@gmai.com", "1234");
+        playerRepository.save(p1);
+
+        PlayerDto dto = new PlayerDto("b", "abcd@gmail.com", "1221", "1221", null);
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> playerService.addNewPlayer(dto));
+        Assertions.assertEquals("Email already in use", exception.getMessage());
+    }
+
+    @Test
+    @Order(2)
+    void PlayerServiceImpl_addNewPlayer_UsedUsername() {
+        RoleService roleService = new RoleServiceImpl(roleRepository);
+        PlayerServiceImpl playerService = new PlayerServiceImpl(playerRepository, roleService, passwordEncoder);
+
+        Player p1 = new Player("a", "abcd@gmai.com", "1234");
+        playerRepository.save(p1);
+
+        PlayerDto dto = new PlayerDto("a", "a@gmail.com", "1221", "1221", null);
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> playerService.addNewPlayer(dto));
+        Assertions.assertEquals("Username already in use", exception.getMessage());
+    }
+
+    @Test
+    @Order(3)
+    void PlayerServiceImpl_addNewPlayer_AddedCorrectly() {
+        RoleService roleService = new RoleServiceImpl(roleRepository);
+        PlayerServiceImpl playerService = new PlayerServiceImpl(playerRepository, roleService, passwordEncoder);
+
+        PlayerDto dto = new PlayerDto("a", "a@gmail.com", "1221", "1221", null);
+
+        Player player = playerService.addNewPlayer(dto);
+
+        Assertions.assertNotNull(player);
+        Assertions.assertEquals(dto.getUsername(), player.getUsername());
+        Assertions.assertEquals(dto.getEmail(), player.getEmail());
+        Assertions.assertTrue(passwordEncoder.matches(dto.getPassword(), player.getPassword()));
+    }
+
+    @Test
+    @Order(4)
+    void PlayerServiceImpl_addNewPlayer_RoleExists() {
+        RoleService roleService = new RoleServiceImpl(roleRepository);
+        PlayerServiceImpl playerService = new PlayerServiceImpl(playerRepository, roleService, passwordEncoder);
+
+        PlayerDto dto = new PlayerDto("a", "a@gmail.com", "1221", "1221", new String[]{"ROLE_USER"});
+        roleService.addRole(new RoleDto(dto.getRoles()[0]));
+
+        Player player = playerService.addNewPlayer(dto);
+
+        Assertions.assertNotNull(player);
+        Assertions.assertEquals(dto.getUsername(), player.getUsername());
+        Assertions.assertEquals(dto.getEmail(), player.getEmail());
+        Assertions.assertTrue(passwordEncoder.matches(dto.getPassword(), player.getPassword()));
     }
 
 }
