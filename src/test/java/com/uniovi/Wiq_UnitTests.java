8ac8de23d067ed9f10eafd6636ae84a595d328d8
uo288061 <uo288061@uniovi.es>
2024-04-02 20:44:55 +0200

    basics units tests for questions generator

--- src/test/java/com/uniovi/Wiq_UnitTests.java
+++ src/test/java/com/uniovi/Wiq_UnitTests.java
@@ -1,13 +1,25 @@
 package com.uniovi;
 
+import com.uniovi.components.generators.geography.BorderQuestionGenerator;
+import com.uniovi.components.generators.geography.CapitalQuestionGenerator;
+import com.uniovi.components.generators.geography.ContinentQuestionGeneration;
 import com.uniovi.entities.Player;
+import com.uniovi.entities.Question;
+import com.uniovi.services.AnswerService;
+import com.uniovi.services.CategoryService;
 import com.uniovi.services.PlayerService;
+import com.uniovi.services.QuestionService;
+import com.uniovi.services.impl.QuestionServiceImpl;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Tag("unit")
@@ -17,11 +29,73 @@ import java.util.List;
 public class Wiq_UnitTests {
     @Autowired
     private PlayerService playerService;
-
+    @Autowired
+    private AnswerService answerService;
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private CategoryService categoryService;
     @Test
     @Order(1)
     public void testPlayerService() {
         List<Player> players = playerService.getUsersByRole("ROLE_USER");
         Assertions.assertEquals(1, players.size());
     }
+    @Test
+    @Order(2)
+    public void testQuestions(){
+        List<Question> questions = questionService.getAllQuestions();
+        Assertions.assertEquals(3802,questions.size());
+
+    }
+    @Test
+    @Order(2)
+    public void testRandomQuestions(){
+        List<Question> questions = questionService.getRandomQuestions(5);
+        Assertions.assertEquals(5,questions.size());
+    }
+
+    @Test
+    @Order(3)
+    public void testBorderQuestionsGenerator(){
+        BorderQuestionGenerator borderQuestionGenerator=new BorderQuestionGenerator(categoryService,Question.SPANISH);
+        List<Question> questions = borderQuestionGenerator.getQuestions();
+        Assertions.assertEquals(855,questions.size());
+
+        for (Question question : questions) {
+            Assertions.assertNotNull(question.getCorrectAnswer());
+            Assertions.assertEquals(4, question.getOptions().size());
+            Assertions.assertTrue(question.getOptions().contains(question.getCorrectAnswer()));
+        }
+    }
+
+    @Test
+    @Order(4)
+    public void testCapitalQuestionsGenerator(){
+        CapitalQuestionGenerator capitalQuestionGenerator=new CapitalQuestionGenerator(categoryService,Question.SPANISH);
+        List<Question> questions = capitalQuestionGenerator.getQuestions();
+        Assertions.assertEquals(206,questions.size());
+
+        for (Question question : questions) {
+            Assertions.assertNotNull(question.getCorrectAnswer());
+            Assertions.assertEquals(4, question.getOptions().size());
+            Assertions.assertTrue(question.getOptions().contains(question.getCorrectAnswer()));
+        }
+    }
+
+    @Test
+    @Order(5)
+    public void testContinentQuestionsGenerator(){
+        ContinentQuestionGeneration continentQuestionGenerator=new ContinentQuestionGeneration(categoryService,Question.SPANISH);
+        List<Question> questions = continentQuestionGenerator.getQuestions();
+        Assertions.assertEquals(207,questions.size());
+
+        for (Question question : questions) {
+            Assertions.assertNotNull(question.getCorrectAnswer());
+            Assertions.assertEquals(4, question.getOptions().size());
+            Assertions.assertTrue(question.getOptions().contains(question.getCorrectAnswer()));
+        }
+    }
+
+
 }
