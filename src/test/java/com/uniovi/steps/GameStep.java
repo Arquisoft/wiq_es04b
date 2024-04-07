32ac3bafafdfc1a96468734b082204e0224ca81a
uo287545 <uo287545@uniovi.es>
2024-04-07 18:01:22 +0200

    Trying to fix error while passing tests on actions

--- src/test/java/com/uniovi/steps/GameStep.java
+++ src/test/java/com/uniovi/steps/GameStep.java
@@ -8,6 +8,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -16,6 +18,7 @@ public class GameStep extends Wiq_IntegrationTests {
 
     @Autowired
     private InsertSampleDataService dataService;
+    private Logger log = LoggerFactory.getLogger(GameStep.class);
 
     @When("I press Play")
     public void iPressPlay() {
@@ -28,10 +31,13 @@ public class GameStep extends Wiq_IntegrationTests {
     public void iShouldStartPlaying() throws InterruptedException {
         boolean playing = true;
         String xpath = "//*[contains(text(),'" + p.getString("game.finish", PropertiesExtractor.getSPANISH()) + "')]";
+        int i= 0;
         List<WebElement> finalMessage;
         while(playing){
             //I obtain the buttons for the answers
             List<WebElement> elems = SeleniumUtils.waitLoadElementsBy(driver, "text", "Test answer 0", 10);
+            log.info("Found " + elems.size() + " buttons");
+            log.info("Iteration " + i + " of the game");
             //I click on the first button
             elems.get(0).click();
 
@@ -43,6 +49,7 @@ public class GameStep extends Wiq_IntegrationTests {
             if(finalMessage.size()>0){
                 playing = false;
             }
+            i++;
         }
     }
 }
