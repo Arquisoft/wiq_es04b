cd6714c74fb756c6a957ebe1f8dbf03c0aec02c9
uo287545 <uo287545@uniovi.es>
2024-04-07 04:43:47 +0200

    Added scenario to check the access to the personal ranking page

--- src/test/java/com/uniovi/steps/LogInStep.java
+++ src/test/java/com/uniovi/steps/LogInStep.java
@@ -1,9 +1,14 @@
 package com.uniovi.steps;
 
 import com.uniovi.Wiq_IntegrationTests;
+import com.uniovi.util.SeleniumUtils;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class LogInStep extends Wiq_IntegrationTests {
     @And("I press the login button")
@@ -22,5 +27,19 @@ public class LogInStep extends Wiq_IntegrationTests {
         password.click();
         password.clear();
         password.sendKeys(pass);
+    }
+
+    @And("I access the personal ranking page")
+    public void iAccessThePersonalRankingPage() {
+        List<WebElement> elems = SeleniumUtils.waitLoadElementsBy(driver, "free", "//*[@id=\"navbarDropdown2\"]", 5);
+        elems.get(0).click();
+        elems = SeleniumUtils.waitLoadElementsBy(driver, "free", "//*[@href=\"/ranking/playerRanking\"]", 5);
+        elems.get(0).click();
+    }
+
+    @Then("I should see the personal ranking page")
+    public void iShouldSeeThePersonalRankingPage() {
+        List<WebElement> elems = SeleniumUtils.waitLoadElementsBy(driver, "@id", "playerRankingTable", 5);
+        Assertions.assertFalse(elems.isEmpty());
     }
 }
