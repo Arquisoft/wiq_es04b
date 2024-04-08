45e00a1a764ed30aa4d8360ddfbb9b3353e3d698
Ricardo Díaz Núñez <UO282909@uniovi.es>
2024-04-08 19:21:23 +0200

    Fix some SonarCloud issues

--- src/main/java/com/uniovi/dto/AnswerDto.java
+++ src/main/java/com/uniovi/dto/AnswerDto.java
@@ -1,6 +1,5 @@
 package com.uniovi.dto;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
--- src/main/java/com/uniovi/entities/Answer.java
+++ src/main/java/com/uniovi/entities/Answer.java
@@ -1,6 +1,5 @@
 package com.uniovi.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
