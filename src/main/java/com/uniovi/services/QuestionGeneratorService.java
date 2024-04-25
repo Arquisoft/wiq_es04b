d829ec906bf629588e23b4b5a8dfecd4805fbb20
uo287545 <uo287545@uniovi.es>
2024-04-25 17:29:03 +0200

    Added stack to store all the different types of questions along with their category

--- src/main/java/com/uniovi/services/QuestionGeneratorService.java
+++ src/main/java/com/uniovi/services/QuestionGeneratorService.java
@@ -3,6 +3,7 @@ package com.uniovi.services;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uniovi.components.generators.QuestionGeneratorV2;
+import com.uniovi.entities.Category;
 import com.uniovi.entities.Question;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,10 +22,32 @@ public class QuestionGeneratorService {
     @Value("${question.json.path}")
     private String jsonFilePath;
 
-    private Stack<JsonNode> types = new Stack<>();
+    private Stack<QuestionType> types = new Stack<>();
+
+    private JsonNode currentType;
 
     public QuestionGeneratorService(QuestionService questionService) {
         this.questionService = questionService;
+        parseQuestionTypes();
+    }
+
+    private void parseQuestionTypes() {
+        try {
+            File jsonFile = new File(jsonFilePath);
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+            JsonNode categories = jsonNode.findValue("categories");
+            for (JsonNode category : categories) {
+                String categoryName = category.get("name").textValue();
+                Category cat = new Category(categoryName);
+                JsonNode questionsNode = category.findValue("questions");
+                for (JsonNode question : questionsNode) {
+                    types.push(new QuestionType(question, cat));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Scheduled(fixedRate = 150000)
@@ -39,12 +62,31 @@ public class QuestionGeneratorService {
             QuestionGeneratorV2 qgen = new QuestionGeneratorV2(jsonNode);
             List<Question> qsp = qgen.getQuestions(Question.SPANISH);
             qsp.forEach(questionService::addNewQuestion);
-            List<Question> qen = qgen.getQuestions(Question.SPANISH);
+            List<Question> qen = qgen.getQuestions(Question.ENGLISH);
             qen.forEach(questionService::addNewQuestion);
-            List<Question> qfr = qgen.getQuestions(Question.SPANISH);
+            List<Question> qfr = qgen.getQuestions(Question.FRENCH);
             qfr.forEach(questionService::addNewQuestion);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private class QuestionType {
+
+        private JsonNode question;
+        private Category category;
+
+        public QuestionType(JsonNode question, Category category) {
+            this.question = question;
+            this.category = category;
+        }
+
+        public JsonNode getQuestion() {
+            return question;
+        }
+
+        public Category getCategory() {
+            return category;
         }
     }
 
