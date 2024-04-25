e5a6f37607203ecfad6e7e6cde169670b31c6cee
uo287545 <uo287545@uniovi.es>
2024-04-25 03:00:08 +0200

    First part of the request system

--- src/main/java/com/uniovi/components/generators/QuestionGeneratorV2.java
+++ src/main/java/com/uniovi/components/generators/QuestionGeneratorV2.java
@@ -1,9 +1,16 @@
 package com.uniovi.components.generators;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uniovi.entities.Category;
 import com.uniovi.entities.Question;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,5 +51,53 @@ public class QuestionGeneratorV2 implements QuestionGenerator{
     private List<Question> generateQuestion(JsonNode question, Category cat) {
         String query = question.get("sparqlQuery").textValue();
         query = query.replace(language_placeholder, language);
+        JsonNode results = getQueryResult(query);
+
+    }
+
+    private JsonNode getQueryResult(String query) {
+
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+
+            String endpointUrl = "https://query.wikidata.org/sparql?query=" +
+                    URLEncoder.encode(this.getQuery(), StandardCharsets.UTF_8) +
+                    "&format=json";
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endpointUrl))
+                    .header("Accept", "application/json")
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Process the JSON response using Jackson ObjectMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonResponse = objectMapper.readTree(response.body());
+
+            // Access the data from the JSON response
+            JsonNode resultsNode = jsonResponse.path("results").path("bindings");
+
+            for (JsonNode result : resultsNode) {
+
+                List<String> options = this.generateOptions(resultsNode, result);
+                String correctAnswer = this.generateCorrectAnswer(result);
+                String questionStatement = this.getQuestionSubject(result);
+
+            }
+        } catch (InterruptedException e) {
+            throw new QuestionGeneratorException("Generation of questions was interrupted");
+        } catch (Exception e) {
+            throw new QuestionGeneratorException("An error occurred while generating questions");
+        }
+
+        return questions;
+
+    }
+
+    private static class QuestionGeneratorException extends RuntimeException {
+        public QuestionGeneratorException(String message) {
+            super(message);
+        }
     }
 }
