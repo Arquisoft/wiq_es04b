e3191167f0a8955b0166830c2d9f0a62ead50c1d
uo287545 <uo287545@uniovi.es>
2024-04-25 03:30:13 +0200

    Added statement generation based on language

--- src/main/java/com/uniovi/components/generators/QuestionGeneratorV2.java
+++ src/main/java/com/uniovi/components/generators/QuestionGeneratorV2.java
@@ -19,12 +19,14 @@ public class QuestionGeneratorV2 implements QuestionGenerator{
     private JsonNode jsonNode;
     private String language_placeholder;
     private String question_placeholder;
+    private String answer_placeholder;
     private String language;
 
     public QuestionGeneratorV2(JsonNode jsonNode, String language) {
         this.jsonNode = jsonNode;
         this.language_placeholder = jsonNode.get("language_placeholder").textValue();
         this.question_placeholder = jsonNode.get("question_placeholder").textValue();
+        this.answer_placeholder = jsonNode.get("answer_placeholder").textValue();
         this.language = language;
     }
 
@@ -49,15 +51,47 @@ public class QuestionGeneratorV2 implements QuestionGenerator{
     }
 
     private List<Question> generateQuestion(JsonNode question, Category cat) {
+        // Get the SPARQL query from the JSON
         String query = question.get("sparqlQuery").textValue();
-        query = query.replace(language_placeholder, language);
+        // Replace the placeholders in the query with the actual values
+        query = query.replace(language_placeholder, language).
+                replace(question_placeholder, question.get("question").textValue()).
+                replace(answer_placeholder, question.get("answer").textValue());
+        // Execute the query and get the results
         JsonNode results = getQueryResult(query);
+        List<Question> questions = new ArrayList<>();
+        // Prepare the statement base based on the language
+        String statement = this.prepareStatement(question);
+        for(JsonNode result : results){
+            String statement = result.get("statement").textValue();
+            List<String> options = new ArrayList<>();
+            for(JsonNode option : result.get("options")){
+                options.add(option.textValue());
+            }
+            String correctAnswer = result.get("correctAnswer").textValue();
+            this.questionGenerator(statement, options, correctAnswer, cat, questions);
+        }
+    }
 
+    /**
+     * Generates a statement based on the language of the question
+     * @param question The question node
+     * @return The statement in the language of the question or null if the language is not found
+     */
+    private String prepareStatement(JsonNode question) {
+        JsonNode statementNode = question.findValue("statements");
+        for (JsonNode statement : statementNode) {
+            if (statement.get("language").textValue().equals(language)) {
+                return statement.get("statement").textValue();
+            }
+        }
+        return null;
     }
 
     private JsonNode getQueryResult(String query) {
 
         HttpClient client = HttpClient.newHttpClient();
+        JsonNode resultsNode;
         try {
 
             String endpointUrl = "https://query.wikidata.org/sparql?query=" +
@@ -76,22 +110,15 @@ public class QuestionGeneratorV2 implements QuestionGenerator{
             JsonNode jsonResponse = objectMapper.readTree(response.body());
 
             // Access the data from the JSON response
-            JsonNode resultsNode = jsonResponse.path("results").path("bindings");
+            resultsNode = jsonResponse.path("results").path("bindings");
 
-            for (JsonNode result : resultsNode) {
-
-                List<String> options = this.generateOptions(resultsNode, result);
-                String correctAnswer = this.generateCorrectAnswer(result);
-                String questionStatement = this.getQuestionSubject(result);
-
-            }
         } catch (InterruptedException e) {
             throw new QuestionGeneratorException("Generation of questions was interrupted");
         } catch (Exception e) {
             throw new QuestionGeneratorException("An error occurred while generating questions");
         }
 
-        return questions;
+        return resultsNode;
 
     }
 
